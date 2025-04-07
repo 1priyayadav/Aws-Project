b8110e41f00815da7096c684c534962ca741e6df
1priyayadav <130156879+1priyayadav@users.noreply.github.com>
2025-04-07 19:54:57 +0530

    Add files via upload

---
+++ fullstack-project/src/App.js
@@ -0,0 +1,13 @@
+import UploadForm from './components/UploadForm';
+
+function App() {
+  return (
+    <div className="bg-gray-50 p-8 h-lvh">
+      <header className="">
+        <UploadForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
